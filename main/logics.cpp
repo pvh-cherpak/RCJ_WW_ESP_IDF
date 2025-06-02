f975492cd3de25c505be1695b70cab59401ebe28
igolovachenko2007@gmail.com <igolovachenko@yandex.ru>
2025-06-02 18:05:02 +0300

    Нападающий без мяча 2

--- main/logics.cpp
+++ main/logics.cpp
@@ -1113,7 +1113,7 @@ void playForwardDribble2(int color)
         int cam_dist = sensor.Cam.gate(color).distance;
 
         int st = sensor.Locator.getStrength();
-        int ballAngle = sensor.Locator.getBallAngleLocal();
+        int ballAngle = 0; //sensor.Locator.getBallAngleLocal();
         int lineAngle = sensor.LineSensor.getAngleDelayed();
 
         //if (sensor.Cam.gate(color).width >= 0)
